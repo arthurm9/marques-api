7d29d12080b9dd14fef494cb5cc0b7311e038df8
Arthur Marques <79710914+arthurm9@users.noreply.github.com>
2022-11-28 20:42:21 -0300

    add insert's into cliente table

--- src/main/resources/db/migration/V001__Criar_Tabela_Cliente.sql
+++ src/main/resources/db/migration/V001__Criar_Tabela_Cliente.sql
@@ -3,3 +3,10 @@ CREATE TABLE IF NOT EXISTS Cliente (
     nome VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
 );
+
+INSERT INTO Cliente(nome) VALUES
+    ("Arteira"),
+    ("Locas"),
+    ("Rojérik"),
+    ("Jovão"),
+    ("Luthur);
